5ed631b4567e180258da63a4fedaeba378f30112
flare criteria <flarecriteria@localhost>
2016-05-10 12:54:29 -0400

    Pangram init

---
+++ pangram/pangram.rb
@@ -0,0 +1,14 @@
+##
+# Checks to see if a string is a pangram
+module Pangram
+  VERSION = 1
+  def is_pangram?(str) # rubocop:disable Style/PredicateName
+    required = ('A'..'Z').to_a
+    (0..str.length - 1).each do |pos|
+      break if required.empty?
+      required.delete(s[pos].upcase)
+    end
+    required.empty?
+  end
+  module_function :is_pangram?
+end
