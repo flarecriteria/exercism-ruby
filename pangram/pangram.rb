5003ce15cf5b5a593cdaa9da8fcbcc441583c296
flare criteria <flarecriteria@localhost>
2016-05-10 12:55:50 -0400

    Prefer with extra whitespace

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -2,6 +2,7 @@
 # Checks to see if a string is a pangram
 module Pangram
   VERSION = 1
+
   def is_pangram?(str) # rubocop:disable Style/PredicateName
     required = ('A'..'Z').to_a
     (0..str.length - 1).each do |pos|
