2e3c13b6f4aa0b2d83860f7e5dbbf3a5b623b7c3
flare criteria <flarecriteria@localhost>
2016-05-10 09:14:46 -0400

    Refractored and commented Hamming. Added Problem
    Changed variable names and added comment in response
    to feedback from @kotp.

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,11 +1,27 @@
-# Calculates Hamming Distance
+##
+# This class calculates the hamming distance
 class Hamming
+  # Problem Version
   VERSION = 2
 
-  def self.compute(s1, s2)
-    raise UnequalLengthError unless s1.length.equal? s2.length
-    s1.chars.zip(s2.chars).count { |a, b| a != b }
+  # Compares two DNA strands of equal length and counts how many of the
+  # nucleotides are different from their equivalent in the other strand.
+  #
+  # An UnequalLengthError is raised if the strands are not the same length.
+  #
+  # ==== Attributes
+  #
+  # * +strand1+ The first dna strand to be compared
+  # * +strand2+ The second dna strand to be compared
+  def self.compute(strand1, strand2)
+    raise UnequalLengthError unless strand1.length.equal? strand2.length
+    strand1.chars.zip(strand2.chars).count { |a, b| a != b }
   end
 end
 
-class UnequalLengthError < ArgumentError; end
+# Raised when strands provided are of unequal lenth
+class UnequalLengthError < ArgumentError
+  def message
+    'Strand lengths are unequal. Strands are expected to have the same length.'
+  end
+end
