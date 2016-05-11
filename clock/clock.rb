df7738706746ae46e6ec653f4177a93fc2f8f2c8
flare criteria <flarecriteria@localhost>
2016-05-11 19:19:53 -0400

    Clock Init

---
+++ clock/clock.rb
@@ -0,0 +1,28 @@
+##
+# Clock
+class Clock
+  # Problem Version
+  VERSION = 1
+
+  def self.at(hours, minutes)
+    Clock.new(hours, minutes)
+  end
+
+  def initialize(hours, minutes)
+    @minutes = 0
+    self + (hours * 60 + minutes)
+  end
+
+  def to_s
+    format('%02d:%02d', *@minutes.divmod(60))
+  end
+
+  def ==(other)
+    to_s == other.to_s
+  end
+
+  def +(other)
+    @minutes = (other + @minutes) % 1440
+    self
+  end
+end
