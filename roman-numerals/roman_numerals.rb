b171003ba16840793313c5d902eaca7c0f5315d8
flare criteria <flarecriteria@localhost>
2016-05-11 11:48:45 -0400

    Better namespacing

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -17,29 +17,30 @@ module RomanNumerals
     I: 1
   }.freeze
 
-  def to_roman
-    remaining = to_i
-    raise ArgumentError unless remaining >= 1
-    raise NotImplementedError unless remaining < 4000
-    # Interestingly, even with string keys, ruby 2.3.0 requires me to use to_s,
-    # or a type error is thrown. I'm not quite sure it's supposed to do that.
-    # So I changed the hash back to symbols, I like the way that looks better
-    # anyways.
-    #
-    # Did not realize each_with_object or divmod existed till I saw
-    # @DebbieGillespie's anwser. Much better
-    ROMAN_NUMERALS.each_with_object('') do |(roman_glyph, value), out|
-      next unless remaining >= value
-      (quotient, remaining) = remaining.divmod(value)
-      out << roman_glyph.to_s * quotient
+  ##
+  # Instance methods live here.
+  # Keep namespaces clean and constants constant
+  module InstanceMethods
+    def to_roman
+      remaining = to_i
+      raise ArgumentError unless remaining >= 1
+      raise NotImplementedError unless remaining < 4000
+      # Interestingly, even with string keys, ruby 2.3.0 requires me to use to_s
+      # or a type error is thrown. I'm not quite sure it's supposed to do that.
+      # So I changed the hash back to symbols, I like the way that looks better
+      # anyways.
+      #
+      # Did not realize each_with_object or divmod existed till I saw
+      # @DebbieGillespie's answer. Much better
+      ROMAN_NUMERALS.each_with_object('') do |(roman_glyph, value), out|
+        next unless remaining >= value
+        (quotient, remaining) = remaining.divmod(value)
+        out << roman_glyph.to_s * quotient
+      end
     end
   end
 end
 
-##
-# Monkey Patch
-class Fixnum
-  # Just no, on a non-namespaced VERSION constant inside of Fixnum.
-  # Even monkey patching a core class for a single use case makes me cringe.
-  include RomanNumerals
-end
+# Still not adding VERSION to fixed num
+# Happier with this
+Fixnum.include(RomanNumerals::InstanceMethods)
