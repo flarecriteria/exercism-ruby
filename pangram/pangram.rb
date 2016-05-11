25ce34727316c4cacd3bfdbd779c49b0b313eb9f
flare criteria <flarecriteria@localhost>
2016-05-10 21:29:15 -0400

    Pangram

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -1,23 +1,33 @@
 ##
-# Checks to see if a string is a pangram
-module Pangram
-  # Problem Version
-  VERSION = 1
-
-  # Determine if a string uses every letter of the alphabet at least once.
+# Adds pangram to any class with to_s
+module PangramRole
+  # Tests to see if self.to_s returns a pangram.
+  # A pangram contains all of the characters in an alphabet at least once.
   #
   # ==== Attributes
   #
-  # * +str+ The string
-  def is_pangram?(str) # rubocop:disable Style/PredicateName
-    required = ('A'..'Z').to_a
-    return false unless str.length >= required.length
+  # * +alphabet+ An array containing the characters of the alphabet used by
+  #   the pangram. Defaults to Latin (A-Z).
+  def pangram?(alphabet = [*'A'..'Z'])
+    str = to_s
+    required = Array.new(alphabet)
+    return false unless str.length > required.length
     (0..str.length - 1).each do |pos|
       break if required.empty?
-      required.delete(s[pos].upcase)
+      required.delete(str[pos].upcase)
     end
     required.empty?
   end
+end
 
+##
+# Wraps PangramRole
+module Pangram
+  VERSION = 1
+
+  def is_pangram?(str) # rubocop:disable Style/PredicateName
+    str.extend(PangramRole)
+    str.pangram?
+  end
   module_function :is_pangram?
 end
