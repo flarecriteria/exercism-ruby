4d08969275812501594bc90a16de84a49c799cc8
flare criteria <flarecriteria@localhost>
2016-05-11 14:07:01 -0400

    Cleaned up with comments from @remcopeereboom

--- sieve/sieve.rb
+++ sieve/sieve.rb
@@ -1,7 +1,7 @@
 ##
 # Implementation of the Sieve of Eratosthenes
 #
-# Why would you makes this an instancialized class?
+# Why would you make this an instancialized class?
 class Sieve
   def initialize(up_to_num)
     @up_to_num = up_to_num
@@ -9,17 +9,18 @@ class Sieve
 
   # Everytime I go to optimize this it's no longer the sieve
   #
-  def primes # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+  def _primes
     arr = Array.new(@up_to_num + 1)
     arr[2] = true unless @up_to_num < 2
     (3..@up_to_num).step(2).each do |num|
       next unless arr[num].nil?
       arr[num] = true
-      prime = num
-      while (num += prime) <= @up_to_num
-        arr[num].nil? && arr[num] = false
-      end
+      (num + num..@up_to_num).step(num) { |i| arr[i] = false }
     end
-    arr.size.times.select { |i| arr[i] }
+    @primes = arr.size.times.select { |i| arr[i] }
+  end
+
+  def primes
+    @primes || _primes
   end
 end
