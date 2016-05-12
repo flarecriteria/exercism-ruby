ff2ac94acbbe9942d0d50033edf10b6c9c915033
flare criteria <flarecriteria@localhost>
2016-05-12 16:04:46 -0400

    82 sighs

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,16 +1,14 @@
-#!/usr/bin/env ruby
-
 ##
 # Converts integer to string
 class Raindrops
   # Program Version
   VERSION = 1
 
-  def self.convert(int)
-    str = ''
-    str += int % 3 == 0 ? 'Pling' : ''
-    str += int % 5 == 0 ? 'Plang' : ''
-    str += int % 7 == 0 ? 'Plong' : ''
-    str.empty? ? int.to_s : str
+  # Didn't know numeric keys get forced old style hash
+  TR = { 3 => 'Pling', 5 => 'Plang', 7 => 'Plong' }.freeze
+
+  # 82 :(
+  def self.convert(i)
+    TR.each_with_object('') { |(k, v), s| s << v if (i % k) == 0 }[/.+/] || i.to_s
   end
 end
