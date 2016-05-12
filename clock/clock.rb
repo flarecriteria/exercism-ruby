0e1c0b360c14a559e457407ef13d858c09fa8f5d
flare criteria <flarecriteria@localhost>
2016-05-12 10:51:36 -0400

    Magic Numbers Bad.

--- clock/clock.rb
+++ clock/clock.rb
@@ -3,17 +3,19 @@
 class Clock
   # Problem Version
   VERSION = 1
+  MINUTES_IN_DAY = 1440
+  MINUTES_IN_HOUR = 60
 
   def self.at(hours, minutes)
-    new(hours * 60 + minutes)
+    new(hours * MINUTES_IN_HOUR + minutes)
   end
 
   def initialize(minutes)
-    @minutes = minutes % 1440
+    @minutes = minutes % MINUTES_IN_DAY
   end
 
   def to_s
-    format('%02d:%02d', *@minutes.divmod(60))
+    format('%02d:%02d', *@minutes.divmod(MINUTES_IN_HOUR))
   end
 
   def ==(other)
